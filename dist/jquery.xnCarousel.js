8ca4e1ccf1fdfc1c9ef9d9ac81929ca27f24f9ed
Juan <jmf@x-team.com>
2014-01-03 19:32:36 -0300

    latest build

--- dist/jquery.xnCarousel.js
+++ dist/jquery.xnCarousel.js
@@ -1899,7 +1899,7 @@ module.exports = Class.extend({
 		var defaults = {
 			touchEnabled: false,
 			pageSize: 1,
-			rangesConfiguration: null,
+			breakpointsConfiguration: null,
 			itemWidth: null,
 			animationType: 'none',
 			loadingType: 'lazy',
@@ -2402,7 +2402,7 @@ module.exports = Class.extend({
 	},
 
 	_updateConfiguration: function(){
-		var configuration = this._getIntervalsProperty(this.settings.rangesConfiguration);
+		var configuration = this._getIntervalsProperty(this.settings.breakpointsConfiguration);
 		var hasToUpdate = false;
 		if (configuration) {
 			if (typeof(configuration.itemWidth) !== 'undefined') {
@@ -2415,8 +2415,8 @@ module.exports = Class.extend({
 			}
 			
 			if (typeof(configuration.pageSize) !== 'undefined') {
-				if (configuration.pageSize !== this.settings.pageSize) {
-					this.settings.pageSize = configuration.pageSize;
+				this.settings.pageSize = configuration.pageSize;
+				if (this.size.unitType === "px") {
 					hasToUpdate = true;
 				}
 			}
